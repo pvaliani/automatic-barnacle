afc93247aaa785884a3de7569bed735d44d59668
Pedram Valiani <p.valiani@gmail.com>
2020-12-08 16:29:03 +0000

    text file updates

--- models/specs/paint_can_spec.js
+++ models/specs/paint_can_spec.js
@@ -22,6 +22,7 @@ describe('Paint_Can', function(){
 
     it("should have a number of litres of paint", function(){
 
+        // using the value of 5 from the new paint_can object
         const actual = paint_can.litres;
         assert.strictEqual(actual, 5)
 
@@ -36,6 +37,7 @@ describe('Paint_Can', function(){
 
     it("should be able to empty itself of paint", function(){
 
+        // apply the emptyPaint method to set paint_can to 0 litres then check against the expected value of 0 
         paint_can.emptyPaint();
         const actual = paint_can.litres;
         assert.strictEqual(actual, 0)
